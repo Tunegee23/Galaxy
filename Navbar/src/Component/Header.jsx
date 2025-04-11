f57850e0f969fa6fea402a8463b11056567aa85d
Tunegee23 <ogunsanya.opeyemi23@gmail.com>
2025-04-11 08:36:25 +0100

    fix

--- Navbar/src/Component/Header.jsx
+++ Navbar/src/Component/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className='md:flex  '>
 
-      <div className='p-3  mx-0'>
+      <div className='p-3 relative top-8 mx-0'>
         <div className='space-y-2 '>
           <p className='text-[#8e8e8e] text-[12px] font-sans-Bold  '>Rate <strong className='text-[#FFFFFF]'>5.0</strong> on Shopify </p>
           <h1 className='text-white leading-[3.2rem] font-sans w-full md:w-3/2 md:text-[60px] md:leading-[4rem] font-[500] text-[45px]'>Meet the <span className=' underline decoration-wavy decoration-[#f8fdaf]'> Super-fast</span>  E-commence Video <span className= 'text-gray-300 '>Platform</span></h1>
@@ -19,8 +19,8 @@ function Header() {
         </div>
 
       </div>
-      <div className='justify-items-center md:1/2  mt-2 '>
-        <img src={png}  className='md:w-[700px] h-[50vh]' ></img>
+      <div className='justify-items-center relative top-8 md:1/2  mt-2 '>
+        <img src={png}  className='md:w-[700px] ' ></img>
       </div>
       
 
